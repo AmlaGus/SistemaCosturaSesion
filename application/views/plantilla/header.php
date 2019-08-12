5f6221ca0ff24f6af3bdb5fd553bde89fddd8fd2
Alma <alma_11de@hotmail.com>
2019-08-11 19:30:57 -0500

    actualizando header

---
+++ application/views/plantilla/header.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>Bienvenido - Admin</title>
+    <link rel="stylesheet" href="<?php echo base_url()?>informacion/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>informacion/css/bootstrap-datepicker3.min.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>informacion/css/dataTables.bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>informacion/css/jquery.dataTables.min.css">
+
+</head>
+
+<body>
+
+<?php $this->load->view('plantilla/menu')?>
